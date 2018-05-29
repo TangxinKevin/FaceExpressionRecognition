2592e552b9ecd052ffeb63977af5ed18a1a93533
Tangxin <tangxint@gmail.com>
2018-05-29 09:25:07 +0800

    Add files via upload

--- train_FER2013.py
+++ train_FER2013.py
@@ -1,8 +1,8 @@
-from src.data_generator import NumpyArrayGenerator,
+from src.data_generator import NumpyArrayGenerator,\
                                DirectoryGenerator
 from src.dataset import FER2013
 from src.data_utils import split_dataset
-from src.network import TransferLearningDeepCNN,
+from src.network import TransferLearningDeepCNN, \
                         ConvolutionalNN, Mini_Xception
 from config import DefaultConfig
 
@@ -12,24 +12,26 @@ from keras.preprocessing.image import ImageDataGenerator
 opt = DefaultConfig()
 raw_image_size = (48, 48)
 data_csv_path = '/home/user/Documents/dataset/fer2013/fer2013.csv'
-csv_label_col = 0
-csv_image_col = 1
+
 
 # Load dataset
 dataset = FER2013(opt.target_emotion_map, raw_image_size, opt.target_image_size,
-                  opt.out_channels, data_csv_path, csv_label_col, csv_image_col)
+                  opt.out_channels, data_csv_path)
 images, labels, emotion_map = dataset.load_data()
+
 
 # split_dataset
 validation_split = 0.1
 trainset, validationset = split_dataset(images, labels, validation_split)
 
-train_image_generator = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=20,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
+train_image_generator = ImageDataGenerator(rescale=1./255,
+                                           rotation_range=10,
+                                           width_shift_range=0.1,
+                                           height_shift_range=0.1,
+                                           shear_range=0.1,
+                                           zoom_range=0.1,
+                                           horizontal_flip=True,
+                                           fill_mode='nearest')
 test_image_generator = ImageDataGenerator(rescale=1./255)
 
 train_gen = NumpyArrayGenerator(trainset[0], trainset[1], train_image_generator,
@@ -39,8 +41,7 @@ validation_gen = NumpyArrayGenerator(validationset[0], validationset[1],
                                      test_image_generator,
                                      batch_size=opt.batch_size)
 
-model = Mini_Xception(opt.input_shape, opt.l2_regularization, 
-                      opt.target_emotion_map)
+model = Mini_Xception(opt.input_shape, opt.target_emotion_map, opt.l2_regularization)
 
 model.fit_generator(train_gen, opt.learning_rate, 'fer2013', 
                     opt.log_file_path, opt.model_path,
