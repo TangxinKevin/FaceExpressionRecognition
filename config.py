9ca4ada5e8915627960cc4e9e84bd8d255705ebb
Tangxin <tangxint@gmail.com>
2018-05-29 09:26:02 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 class DefaultConfig():
-    target_emotion_map = {'0': 'anger', '1': 'fear',
-                          '2': 'happy', '3': 'sadness', '4': 'surprise',
-                          '5': 'neutral'}
+    target_emotion_map = {'anger': 0, 'fear': 1,
+                          'happy': 2, 'sadness': 3,
+                          'surprise': 4, 'neutral': 5}
     target_image_size = (64, 64)
     out_channels = 3
     batch_size = 64
     input_shape = (64, 64, 3)
     l2_regularization = 0.001
-    learning_rate = 0.001
-    log_file_path = '../log/'
-    model_path = '../model/'
+    learning_rate = 0.0001
+    log_file_path = '/home/user/Documents/delta/expression/FaceExpressionRecognition-master/log'
+    model_path = '/home/user/Documents/delta/expression/FaceExpressionRecognition-master/model'
     epochs = 1000
